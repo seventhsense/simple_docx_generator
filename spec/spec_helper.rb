b238edfa4cd5c85a4320c8896880850decad93dd
seventhsense <seven@lh4l.com>
2012-11-10 14:27:22 +0900

    get coverage 100%

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'simplecov'
-require 'simplecov-vim/formatter'
-SimpleCov.start do
-  formatter SimpleCov::Formatter::VimFormatter
-end
+# require 'simplecov-vim/formatter'
+# SimpleCov.start do
+  # formatter SimpleCov::Formatter::VimFormatter
+# end
+SimpleCov.start
 require 'simple_docx_generator'
