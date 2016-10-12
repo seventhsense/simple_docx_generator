3d2178a0107281777e05fea68f614b8e606d14f9
seventhsense <seventh@scimpr.com>
2016-10-13 08:09:38 +0900

    update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,8 @@ require 'simplecov'
 require 'simplecov-vim/formatter'
 require 'codeclimate-test-reporter'
 require 'simple_docx_generator'
-CodeClimate::TestReporter.start
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
   SimpleCov::Formatter::VimFormatter,
   CodeClimate::TestReporter::Formatter
 ]
+SimpleCov.start
